f469701ab19577f5d041e7abecb1c981df2506aa
Ryan Owens <RyanOwens@linux.com>
2016-12-20 23:00:20 -0600

    Don't need the full namespace in the manager

--- src/VirtMan/VirtMan.php
+++ src/VirtMan/VirtMan.php
@@ -3,24 +3,24 @@
 namespace Ryanvade\VirtMan;
 
 // Commands
-use Ryanvade\VirtMan\Command\AddStorage;
-use Ryanvade\VirtMan\Command\StopMachine;
-use Ryanvade\VirtMan\Command\AddToNetwork;
-use Ryanvade\VirtMan\Command\CloneMachine;
-use Ryanvade\VirtMan\Command\CloneStorage;
-use Ryanvade\VirtMan\Command\StartMachine;
-use Ryanvade\VirtMan\Command\CreateMachine;
-use Ryanvade\VirtMan\Command\CreateNetwork;
-use Ryanvade\VirtMan\Command\CreateStorage;
-use Ryanvade\VirtMan\Command\DeleteMachine;
-use Ryanvade\VirtMan\Command\DeleteNetwork;
-use Ryanvade\VirtMan\Command\DeleteStorage;
+use Command\AddStorage;
+use Command\StopMachine;
+use Command\AddToNetwork;
+use Command\CloneMachine;
+use Command\CloneStorage;
+use Command\StartMachine;
+use Command\CreateMachine;
+use Command\CreateNetwork;
+use Command\CreateStorage;
+use Command\DeleteMachine;
+use Command\DeleteNetwork;
+use Command\DeleteStorage;
 
 // Models
-use Ryanvade\VirtMan\Group\Group;
-use Ryanvade\VirtMan\Machine\Machine;
-use Ryanvade\VirtMan\Network\Network;
-use Ryanvade\VirtMan\Storage\Storage;
+use Group\Group;
+use Machine\Machine;
+use Network\Network;
+use Storage\Storage;
 
 class VirtMan {
 
