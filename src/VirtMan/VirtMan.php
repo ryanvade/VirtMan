1e1e0ac3597d9021773f43649338c494639accd7
Ryan Owens <RyanOwens@linux.com>
2016-12-20 22:58:52 -0600

    File cleanup

--- src/VirtMan/VirtMan.php
+++ src/VirtMan/VirtMan.php
@@ -4,17 +4,17 @@ namespace Ryanvade\VirtMan;
 
 // Commands
 use Ryanvade\VirtMan\Command\AddStorage;
+use Ryanvade\VirtMan\Command\StopMachine;
 use Ryanvade\VirtMan\Command\AddToNetwork;
 use Ryanvade\VirtMan\Command\CloneMachine;
 use Ryanvade\VirtMan\Command\CloneStorage;
+use Ryanvade\VirtMan\Command\StartMachine;
 use Ryanvade\VirtMan\Command\CreateMachine;
 use Ryanvade\VirtMan\Command\CreateNetwork;
 use Ryanvade\VirtMan\Command\CreateStorage;
 use Ryanvade\VirtMan\Command\DeleteMachine;
 use Ryanvade\VirtMan\Command\DeleteNetwork;
 use Ryanvade\VirtMan\Command\DeleteStorage;
-use Ryanvade\VirtMan\Command\StartMachine;
-use Ryanvade\VirtMan\Command\StopMachine;
 
 // Models
 use Ryanvade\VirtMan\Group\Group;
@@ -25,7 +25,7 @@ use Ryanvade\VirtMan\Storage\Storage;
 class VirtMan {
 
   public function __construct() {
-    
+
   }
 
 }
